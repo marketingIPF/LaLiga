a1c894792cbbd0687264f0a2c25ec81006f3f9fe
MarketingIPF <marketing@inmobiliariapalanca.com>
2026-07-28 13:10:17 +0200

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -149,8 +149,8 @@ export const ACTION_TYPES = {
     points: 5,
     icon: '📱',
     description: 'Publicación en tu Instagram / WhatsApp.',
-    leagues: ['agentes', 'staff'],
-    selfService: ['agentes', 'staff'],
+    leagues: ['agentes', 'obranueva', 'staff'],
+    selfService: ['agentes', 'obranueva', 'staff'],
   },
   grabar_reel: {
     id: 'grabar_reel',
